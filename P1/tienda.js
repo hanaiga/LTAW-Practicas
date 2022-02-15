665f468cda5ce931841449a67bad8fd0f717fb0d
hanaig <h.igalla.2018@alumnos.urjc.es>
2022-02-15 22:40:19 +0100

    empezando la tienda

---
+++ P1/tienda.js
@@ -0,0 +1,46 @@
+const http = require('http');
+const fs = require('fs');
+
+const PUERTO = 9090;
+
+//-- Fichero HTML de la página principal
+
+const pagina_main = fs.readFileSync('error.html');
+
+//-- Fichero HTML de la página de error
+const pagina_error = fs.readFileSync('error.html');
+
+
+const server = http.createServer((req, res)=>{
+    console.log("Petición recibida!");
+
+    //-- Valores de la respuesta por defecto
+    let code = 200;
+    let code_msg = "OK";
+    let page = pagina_main;
+
+    //-- Analizar el recurso
+    //-- Construir el objeto url con la url de la solicitud
+    const url = new URL(req.url, 'http://' + req.headers['host']);
+    console.log(url.pathname);
+
+    //-- Cualquier recurso que no sea la página principal
+    //-- genera un error
+    if (url.pathname != '/') {
+        code = 404;
+        code_msg = "Not Found";
+        page = pagina_error;
+    }
+
+    //-- Generar la respusta en función de las variables
+    //-- code, code_msg y page
+    res.statusCode = code;
+    res.statusMessage = code_msg;
+    res.setHeader('Content-Type','text/html');
+    res.write(page);
+    res.end();
+});
+
+server.listen(PUERTO);
+
+console.log("Ejemplo 7. Escuchando en puerto: " + PUERTO);
