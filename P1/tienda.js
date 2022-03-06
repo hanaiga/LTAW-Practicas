f6aa4ad10df3d0972cec45e9f628cedd47af70b7
hanaig <h.igalla.2018@alumnos.urjc.es>
2022-03-06 03:39:01 +0100

    terminado servidor

--- P1/tienda.js
+++ P1/tienda.js
@@ -3,77 +3,76 @@
 const fs = require('fs');
 const http = require('http');
 
-//-- Puerto donde escuha el servidor
-const PUERTO = 9090;
 
-//-- Texto HTML de la página de error
-const pagina_error = fs.readFileSync('error.html');
+//-- Puerto del servidor 
+const PUERTO = 9090; 
 
-//-- Creo el servidor y establezco la retrollamada
-const server = http.createServer((req, res)=>{
+//-- Creamos el servidor
+const server = http.createServer(function(req, res) {
+    
+  //-- Hemos recibido una solicitud
+  console.log("Petición recibida!");
 
-    //-- Recibida una peticion
-    console.log("Petición recibida!");
+  //-- Obtenemos la URL del recurso
+  let url = new URL(req.url, 'http://' + req.headers['host']);
+  console.log("La URL del recurso solicitado es: " + url.href)
 
-    //-- Analizar el recurso y obtengo la URL del mensaje de solicitud
-    //-- Construir el objeto url con la url de la solicitud
-    let url = new URL(req.url, 'http://' + req.headers['host']);
-    console.log(" URL solicitada: " + url.href)
-    console.log(url.pathname)
+    //-- Aqui almaceno el recurso solicitado
+  let recurso = "";
 
-    //-- Aqui almaceno el recurso pedido
-    let recurso = ""
+  //-- Analizo
+  //-- si es el recurso raiz devuelvo la pag principal
+  if(url.pathname == '/') { 
+    recurso += "/tienda.html" 
+  } else { 
+    recurso = url.pathname;
+  }
+  
+  //-- obtengo la extension del recurso
+  extension = recurso.split(".")[1]; 
+  recurso = "." + recurso 
 
-    //-- Analizo la solicitud
-    //-- Si es el recurso raiz devuelvo la pag principal de la tienda
-    if (url.pathname == '/') {
-        recurso += "tienda.html"
-    }else{ //-- otro, me quedo con su recurso
-        recurso += url.pathname
+  console.log("Recurso solicitado: " + recurso);
+  console.log("Extension del recurso: " + extension);
+
+  //-- Defino tipos de mime
+  const type_mime = {
+    "html" : "text/html",
+    "css" : "text/css",
+    "jpeg" : "image/jpeg",
+    "jpg" : "image/jpg",
+    "png" : "image/png",
+    "gif" : "image/gif",
+    "ico" : "image/ico",
+  }; 
+
+  //-- Defino tipo de mime del recurso solicitado
+  let mime = type_mime[extension];
+
+  fs.readFile(recurso, function(err,data) {
+    //-- Si se produce error muestro pag de error
+    if(err) {
+
+        //-- Mandamos cabecera de error
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      console.log("404 Not Found");
+      recurso = "html/error.html";
+      data = fs.readFileSync(recurso);
+       
+    }else { 
+        //-- Mandamos cabecera de ok
+      res.writeHead(200, {'Content-Type': mime});
+      console.log("200 OK")
     }
-
-    //-- la extension del recurso
-    extension = recurso.split(".")[1]
-
-    //-- Defino los tipos de mime
-    const type_mime = {
-        "html" : "text/html",
-        "css"  : "text/css",
-        "jpg"  : "image/jpg",
-        "JPG"  : "image/jpg",
-        "jpeg" : "image/jpeg",
-        "png"  : "image/png",
-        "gif"  : "image/gif",
-        "ico"  : "image/x-icon"
-    }
-
-    //-- Defino el tipo de mime del recurso
-    let mime = type_mime[extension]
-
-    //-- Leo el fichero
-    fs.readFile(recurso, function(err, data){
-
-        //-- Si se produce error, muestro la pag de error
-        if (err){
-            //-- La cabecera 404 de error
-            res.writeHead(404, {'Content-Type': 'text/html'})
-            console.log("404 Not Found")
-            data = pagina_error
-
-        //-- Ningun error, cabecera de 200 ok     
-        }else{
-            res.writeHead(200, {'Content-Type': mime})
-            console.log("200 OK")
-        }
-
-        //-- Envio el recurso solicitado 
-        res.write(data)
-        res.end()
-
-    })
-})
-
+    
+    // Enviamos el recurso solicitado
+    res.write(data);
+    res.end();
+  });
+   
+});
+  
+//-- Activo el servidor:
 server.listen(PUERTO);
 
-console.log("Escuchando en puerto: " + PUERTO);
-
+console.log("Server tienda activado!. Escuchando en puerto: " + PUERTO);
